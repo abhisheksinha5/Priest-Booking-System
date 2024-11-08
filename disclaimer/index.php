a9450d08508b3b7203fd0cda4e8ce007c0e925d2
Abhishek Kumar Sinha <99522314+abhisheksinha5@users.noreply.github.com>
2024-11-08 08:33:02 +0530

    Add files via upload

---
+++ disclaimer/index.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+<meta charset="UTF-8">
+<title>Priests-Login/Register</title>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;900&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link href="../login/css/styles.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+
+<script src="https://use.fontawesome.com/releases/v6.0.0/js/all.js"></script>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+  <section class="colored-section" id="title">
+    <div class="container-fluid">
+      <!-- Nav Bar -->
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="../landing/index.php">Priests</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="../landing/index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../landing/#features">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../landing/#cta">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../priest_login/index.php"> <strong>Register As Priest</strong> </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../login/index.php"><strong> Login/SignUp</strong></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+  </section>
+
+
+<section id="disclaimer" style="margin:5rem 10rem">
+
+<br> <h2>Disclaimers for Priests</h2>
+
+<p>All the information on this website - priests.com - is published in good faith and for general information purpose only. Priests does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (Priests), is strictly at your own risk. Priests will not be liable for any losses and/or damages in connection with the use of our website.</p>
+
+<p>From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone 'bad'.</p>
+
+<p>Please be also aware that when you leave our website, other sites may have different privacy policies and terms which are beyond our control. Please be sure to check the Privacy Policies of these sites as well as their "Terms of Service" before engaging in any business or uploading any information.</p>
+
+<br><h2>Consent</h2>
+
+<p>By using our website, you hereby consent to our disclaimer and agree to its terms.</p>
+
+<br><h2>Update</h2>
+
+<p>Should we update, amend or make any changes to this document, those changes will be prominently posted here.</p>
+
+  </section>
+
+
+
+
+<!-- partial -->
+</body>
+</html>
