0964535bbe1729112e5a8bf27ea6cafdae71833d
Abhishek Kumar Sinha <99522314+abhisheksinha5@users.noreply.github.com>
2024-11-08 08:33:26 +0530

    Add files via upload

---
+++ Landing/index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Priests</title>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;900&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- CSS stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="css/styles.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+
+    <!-- Font Awesome -->
+    <script src="https://use.fontawesome.com/releases/v6.0.0/js/all.js"></script>
+    <!-- BootStrap Scripts -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <section class="colored-section" id="title">
+      <div class="container-fluid ">
+        <!-- Nav Bar -->
+        <nav class="navbar navbar-expand-lg navbar-dark">
+          <a class="navbar-brand" href="">Priests</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ml-auto">
+
+              <li class="nav-item">
+                <a class="nav-link" href="#features">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#cta">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#areas">Areas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../priest_login/index.php"><strong>Join as Priest</strong></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../login/index.php"><strong> Login/SignUp</strong></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <!-- Title -->
+        <div class="row">
+          <div class="col-lg-6">
+            <h1 class="big-heading">Worship is act of war against enemy of our hearts.</h1><br><br>
+            <a class="btn btn-dark btn-lg know-more-button" href="../login/index.php" >Book Priest</a>
+            <a class="btn btn-outline-light btn-lg know-more-button" href="#cta" >Contact Us</a>
+
+          </div>
+          <div class="col-lg-6">
+            <img class="title-img" src="images/ganesh3.png" alt="ganesh">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Features -->
+    <section class="white-section" id="features">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="feature-box col-lg-4">
+            <i class="icon fa-solid fa-heart fa-4x"></i>
+            <h3 class="feature-title">Deep Worship</h3>
+            <p>Get priest for deep heart worship.</p>
+          </div>
+          <div class="feature-box col-lg-4">
+            <i class="icon fa-solid fa-bullseye fa-4x"></i>
+            <h3 class="feature-title">Always on Hand</h3>
+            <p>Get skilled and experienced Priests.</p>
+          </div>
+          <div class="feature-box col-lg-4">
+            <i class="icon fa-solid fa-circle-check fa-4x"></i>
+            <h3 class="feature-title">Trustful</h3>
+            <p>Fully friendly and trustful website.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Testimonials -->
+    <section class="colored-section" id="testimonials">
+      <div id="testimonial-carousel" class="carousel slide" data-ride="false">
+        <div class="carousel-inner">
+          <div class="carousel-item active container-fluid">
+            <h2 class="testomonial-text">That which we call the Hindu religion is really the Eternal religion because it embraces all others.</h2>
+            <em>- Aurobindo</em>
+          </div>
+
+          <div class="carousel-item container-fluid">
+            <h2 class="testomonial-text">To worship God is to humble everything about ourselves and exalt everything about him.</h2>
+            <em>- Bob Kauflin</em>
+          </div>
+
+          <div class="carousel-item container-fluid">
+            <h2 class="testomonial-text">I need to worship because without it I lose a sense of wonder and gratitude and plod through life with blinders on.</h2>
+            <em>- John Ortberg</em>
+          </div>
+
+        <div class="carousel-item container-fluid">
+          <h2 class="testomonial-text">Worship is a lifestyle. What you do throughout the day is your act of worship to God.</h2>
+
+          <em>- Patrick Mabilog</em>
+        </div>
+        <div class="carousel-item container-fluid">
+          <h2 class="testomonial-text">Silent worship is an opportunity to seek one's inner spirit and find a powerful sustenance.</h2>
+
+          <em>- Star Staff</em>
+        </div>
+        </div>
+        <a class="carousel-control-prev" href="#testimonial-carousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#testimonial-carousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+      </div>
+    </section>
+    <!-- Press -->
+    <section class="colored-section" id="press">
+      <img class="press-logo" src="images/ganesh1.png" alt="press_ganesh">
+
+    </section>
+    <!-- achievements -->
+    <section class="white-section" id="areas">
+      <h2 class="section-heading">Our Areas Covered</h2>
+      <p>We Are Covering Areas Rapidly</p>
+      <div class="row">
+        <div class="achievements-column col-lg-4 col-md-6">
+          <div class="card">
+            <div class="card-header">
+              <h3>Rayagada</h3>
+            </div>
+            <div class="card-body">
+              <p>Bissam Cuttack</p>
+              <p>Gunupur</p>
+              <p>Muniguda</p>
+              <p>Kujendri</p>
+              <a href="#areas" class="btn btn-lg btn-block btn-outline-dark">Know More</a>
+            </div>
+          </div>
+        </div>
+        <div class="achievements-column col-lg-4 col-md-6">
+          <div class="card">
+            <div class="card-header">
+              <h3>Bargarh</h3>
+            </div>
+            <div class="card-body">
+              <p>Ambabhona</p>
+              <p>Bargarh</p>
+              <p>Sohella</strong></p>
+              <p>Bhatli</p>
+              <a href="#areas" class="btn btn-lg btn-block btn-dark">Know More</a>
+            </div>
+          </div>
+        </div>
+        <div class="achievements-column col-lg-4">
+          <div class="card">
+            <div class="card-header">
+              <h3>Bolangir</h3>
+            </div>
+            <div class="card-body">
+              <p>Dudukasira</p>
+              <p>Janakpur</p>
+              <p>Pipalkani</p>
+                <p>Rajamunda</p>
+                <p>Pipalkani</p>
+              <a href="#areas" class="btn btn-lg btn-block btn-dark">Know More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Call to Action -->
+    <section class="colored-section" id="cta">
+      <div class="container-fluid">
+        <h3 class="big-heading">Great opportunity for the priest to join with us and earn by worshiping.</h3>
+        <button type="button" class="cta-icon btn btn-dark btn-lg know-more-button" href="mailto:priests01@gmail.com">
+          Send us Mail</button>
+
+
+      </div>
+    </section>
+      <section id="footer">
+    <div class="container">
+      <div class="row text-center text-xs-center text-sm-left text-md-left">
+        <div class="col-xs-12 col-sm-4 col-md-4">
+                       <h3 style="color:#4E944F">Priests</h3>
+                       <p>Priests is a 'Priest Booking System', which provides skilled
+                       and verified priests by our Head Priest. We also provide a friendly
+                     User Interface and Secure Environment. To Get convinient access join Us.</p>
+                   </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5 style="color:#4E944F">Contact Us</h5>
+          <ul class="list-unstyled quick-links">
+            <li>Gunupur , Odisha</a></li>
+            <li>765022</a></li>
+            <li>India</a></li>
+            <li>Email : priests@gmail.com</a></li>
+
+          </ul>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4">
+          <h5 style="color:#4E944F">Quick links</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href="#"></i>Home</a></li>
+            <li><a href="#features"></i>About</a></li>
+            <li><a href="../privacy/index.php" target="_blank"></i>Privacy</a></li>
+            <li><a href="../disclaimer/index.php"></i>Disclaimer</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+          <ul class="list-unstyled list-inline social text-center">
+            <a href="#"><i class="social-icon  fa-brands fa-twitter " style="margin-bottom: 0;"></i></a>
+            <a href="#"><i class="social-icon  fa-brands fa-google " style="margin-bottom: 0;"></i></a>
+            <a href="#"><i class="social-icon  fa-solid fa-envelope" style="margin-bottom: 0;"></i></i></a>
+          </ul>
+        </div>
+        </hr>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+          <p>© Copyright 2022 <a href="#">Priests</a></p>
+        </div>
+        </hr>
+      </div>
+    </div>
+  </section>
+  <!-- ./Footer -->
+
+    <a href="#" class="top">Back to Top &#8593;</a>
+  </body>
+</html>
